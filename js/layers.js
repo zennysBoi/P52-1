4c6c8aa1fb01455fdb615c884ee3673ef3e4dcbe
zennysBoi <whalebanana54@gmail.com>
2026-02-17 00:40:40 -0600

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -1,15 +1,15 @@
-addLayer("p", {
-    name: "prestige", // This is optional, only used in a few places, If absent it just uses the layer id.
-    symbol: "P", // This appears on the layer's node. Default is the id with the first letter capitalized
+addLayer("Seconds", {
+    name: "Seconds", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "S", // This appears on the layer's node. Default is the id with the first letter capitalized
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     startData() { return {
         unlocked: true,
 		points: new Decimal(0),
     }},
-    color: "#4BDC13",
+    color: "#a86fdd",
     requires: new Decimal(10), // Can be a function that takes requirement increases into account
-    resource: "prestige points", // Name of prestige currency
-    baseResource: "points", // Name of resource prestige is based on
+    resource: "Elapsed Seconds", // Name of prestige currency
+    baseResource: "seconds", // Name of resource prestige is based on
     baseAmount() {return player.points}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
     exponent: 0.5, // Prestige currency exponent
@@ -24,5 +24,13 @@ addLayer("p", {
     hotkeys: [
         {key: "p", description: "P: Reset for prestige points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
     ],
+    upgrades: {
+        11: {
+            title: "I'm Bored",
+            description: "Take up card shuffling, inadvertantly wasting an additional second every second. [fix later]",
+            cost: new Decimal(10),
+        },
+    },
     layerShown(){return true}
+    
 })
